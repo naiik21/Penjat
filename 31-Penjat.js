8dea4d53834dca7ddce618892558efe497784fd8
naiik21 <nratia22@jaumebalmes.net>
2023-11-06 17:03:53 +0100

    versió final amb comentaris

--- 31-Penjat.js
+++ 31-Penjat.js
@@ -1,3 +1,4 @@
+// Variables
 let paraula="";
 let enigme = "";
 let enigmeS = "";
@@ -8,9 +9,13 @@ let perdudes = 0;
 let errors = 0;
 let abecedari;
 var caractersPermesos = 'ABCÇDEFGHIJLMNÑOPKRSTUVWXYZ';
+let totalGuanyades = parseInt(localStorage.getItem('totalGuanyades')) || 0;
+let totalPerdudes = parseInt(localStorage.getItem('totalPerdudes')) || 0;
 
 
+// Funció que inicia el joc 
 function iniciarJoc() {
+    //Varaibles
     lletresFallades = [];
     lletresEncertades = [];
     errors = 0;
@@ -22,6 +27,7 @@ function iniciarJoc() {
     imagen.src = "";
     document.getElementById("lletresUtilitzades").innerHTML=" ";
     
+    // Demanem la paraula que volem endivinar
     do {
         paraula = prompt("Diguem una paraula: ");
     } while (!paraula.match(/^[A-Z]/gi) || paraula.includes(" "));
@@ -31,31 +37,34 @@ function iniciarJoc() {
     }
     enigmeS = enigme.split("");
     document.getElementById("jocPenjat").innerHTML = enigmeS.join(" ");
-    //document.write(enigmeS.join(" "));
 
+    // Creem un boton de per cada una de les lletres 
     for (var i = 0; i < caractersPermesos.length; i++) {
         var caracter = caractersPermesos[i];
         abecedari += '<button onclick="clickLletra(\'' + caracter + '\')">' + caracter + '</button>';
     }
     document.getElementById("abecedari").innerHTML = abecedari;
 
+    // Enseñem la forma del penjat, depenent del numero d'errors
     var imagen = document.getElementById("imatgePenjat");
     imagen.src = "imatges/penjat_" + errors + ".png";
 }
 
 
+// Funció que ens mostra les estadistiques de les partides 
 function estadistiques() {
-    localStorage.setItem("guanyades", guanyades);
-    localStorage.setItem("perdudes", perdudes);
-    let partidesTotal = guanyades + perdudes;
+    let partidesTotal = totalGuanyades + totalPerdudes;
     let estats = window.open("", "_blank");
     estats.document.write('<p>Total de partides: '+ partidesTotal + 
-                        '</p><br><p>Partides guayades('+ ((localStorage.getItem("guanyades")* 100) / partidesTotal).toFixed(2) + "%): " + localStorage.getItem("guanyades")+
-                        '</p><br><p>Partides perdudes(' + ((localStorage.getItem("perdudes") * 100) / partidesTotal).toFixed(2) + "%): " + localStorage.getItem("perdudes") + '</p>');
+                        '</p><br><p>Partides guayades('+ ((totalGuanyades* 100) / partidesTotal).toFixed(2) + "%): " + totalGuanyades+
+                        '</p><br><p>Partides perdudes(' + ((totalPerdudes * 100) / partidesTotal).toFixed(2) + "%): " + totalPerdudes + '</p>');
 }
 
+
+// Funció procesa la lletre que hem fet click
 function clickLletra(lletre) {
     abecedari="";
+    // Comprobem que la lletra estigui en la paraula
     if (paraula.includes(lletre)) {
         for (let i = 0; i < enigme.length; i++) {
             if (lletre == paraula.charAt(i)) {
@@ -71,13 +80,12 @@ function clickLletra(lletre) {
         
     }
 
-    //console.log(errors);
+    // LLetres errones
     enigmeS = enigme.split("");
     document.getElementById("jocPenjat").innerHTML = enigmeS.join(" ");
     document.getElementById("lletresUtilitzades").innerHTML = lletresFallades;
 
-
-
+    // Bloquejem les lletres ja clicades
     for (var i = 0; i < caractersPermesos.length; i++) {
         var caracter = caractersPermesos[i];
         if(lletresFallades.includes(caracter) || lletresEncertades.includes(caracter)){
@@ -89,14 +97,11 @@ function clickLletra(lletre) {
     document.getElementById("abecedari").innerHTML = " ";
     document.getElementById("abecedari").innerHTML = abecedari;
 
-
-
-
-
-
-
+    // Control de finial de joc
     if (!enigme.includes("_")) {
-        guanyades++;
+        totalGuanyades++;
+        localStorage.setItem('totalGuanyades', totalGuanyades);
+        perdudes++;
         setTimeout(function() {alert("Enhorabona ha encertat la paraula");}, 50);
         paraula;
         enigme = "";
@@ -107,6 +112,8 @@ function clickLletra(lletre) {
         document.getElementById("abecedari").innerHTML=" ";
     }
     if (errors > 5) {
+        totalPerdudes++;
+        localStorage.setItem('totalPerdudes', totalPerdudes);
         perdudes++;
         setTimeout(function() {alert("Has perdut, no has encertat la paraula");}, 50);
         paraula;
